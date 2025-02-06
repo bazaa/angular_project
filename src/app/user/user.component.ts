9a9b795a37c8ed27c50b1b98e97672176a1a902a
bazaa <bazaalejandro7@gmail.com>
2025-02-05 23:30:36 -0500

    changes to user component and app styles file

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
+  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
